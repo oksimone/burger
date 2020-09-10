e61dfe78f85fc33d79c82756d836c84d6addb8f5
ksimone <ksworthy@windstream.net>
2020-09-10 18:54:12 -0400

    final touches

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -36,7 +36,7 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+    
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -49,7 +49,7 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+    
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -57,5 +57,4 @@ router.delete("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
